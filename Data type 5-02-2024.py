26915ef760c64293bf69bb4139062746af757197
Julian David Ramirez <143161834+JulianDavidRamirez@users.noreply.github.com>
2024-02-26 00:14:04 -0500

    Add files via upload

---
+++ Data type 5-02-2024.py
@@ -0,0 +1,49 @@
+# Tipo de datos 
+
+print("\n \t Tipos de datos \n ")
+
+x = 5
+print(type(x))
+
+x = "Hello world"
+print(type(x))
+
+x = 20.5
+print(type(x))
+
+x = ["apple","banana","cherry"]
+print(type(x))
+
+x = ("apple","banana","cherry")
+print(type(x))
+
+x = {"Name": "Julian","Age":"52"}
+print(type(x))
+
+x = 3+5j
+print(type(x))
+
+# Conversiones de datos 
+
+print("\n \t Tipos de datos \n ")
+
+x = 1
+y = 2.8
+z = 1j
+
+# Convertir entero a float:
+a = float(x)
+
+# Convertir de float a entero:
+b = int(y)
+
+# Convertir de entero a complejo:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
